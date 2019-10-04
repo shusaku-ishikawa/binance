aa2d23b08d3b7046d419bd08b2bb3b9c665bcdbb
Shusaku <ishikawasyuusaku@gmail.com>
2019-10-04 23:32:59 +0900

    a

--- frontend/config/prod.env.js
+++ frontend/config/prod.env.js
@@ -1,5 +1,5 @@
 'use strict'
 module.exports = {
   NODE_ENV: '"production"',
-  API_ROOT: '"http://h203-183-9-39.ablenetvps.ne.jp/"'
+  API_ROOT: '"http://h203-183-9-39.ablenetvps.ne.jp/binance/api/"'
 }
